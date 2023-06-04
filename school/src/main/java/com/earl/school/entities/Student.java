78a9d3b388c5929df6b8915693bcbf3e554d6ec8
Earl Harris <esharris@mac.com>
2023-06-04 13:32:20 -0400

    In the Student entity class, made studentId a unique column.

--- school/src/main/java/com/earl/school/entities/Student.java
+++ school/src/main/java/com/earl/school/entities/Student.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@Column(unique = true)
 	private String studentId;
 	private String firstName;
 	private String lastName;
